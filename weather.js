464ce05678edefa7745f1e3265b42bd879b710e8
smarko-web <smarko4web@gmail.com>
2024-05-08 15:55:17 -0500

    fix url

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function getWeather(lat, lon, timezone) {
     return axios.get(
-    "https://api.open-meteo.com/v1/forecast?&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,precipitation_probability,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_probability_max&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FChicago",
+    "https://api.open-meteo.com/v1/forecast?&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,precipitation_probability,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_probability_max&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime",
         { 
             params: {
                 latitude: lat,
